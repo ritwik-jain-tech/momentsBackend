255f3a1956f71fd1d445b03e19506034ec641735
rtwk1001 <rtwk1001@gmail.com>
2025-12-16 20:28:21 +0530

    remove CORS sec from bulk endpoint

--- src/main/java/com/moments/controller/FileUploadController.java
+++ src/main/java/com/moments/controller/FileUploadController.java
@@ -23,7 +23,7 @@ import java.util.concurrent.ExecutionException;
 @RestController
 @RequestMapping("/api/files")
 @CrossOrigin(
-    originPatterns = {"*", "https://admin.moments.live"},
+    originPatterns = {"https://admin.moments.live"},
     allowedHeaders = {"*"},
     methods = {RequestMethod.POST, RequestMethod.OPTIONS, RequestMethod.GET, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.DELETE, RequestMethod.HEAD}
 )
